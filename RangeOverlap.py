7758aca251fba29df10114a80fbf6678b2407921
TonyDeng0514 <89807470+TonyDeng0514@users.noreply.github.com>
2021-09-01 12:50:42 -0400

    Update RangeOverlap.py

--- RangeOverlap.py
+++ RangeOverlap.py
@@ -1,2 +1,12 @@
 def rangeOverlap(x0: int, x1: int, x2: int, x3: int) -> bool:
     assert x0 <= x1 and x2 <= x3
+    
+    # min1<=max2 and min2<=max1
+    
+    if (x1 >= x2 and  x3 >= x0):
+        return True
+    else:
+        return False
+
+assert rangeOverlap(0, 3, 6, 8) == False
+assert rangeOverlap(0, 6, 3, 8) == True
