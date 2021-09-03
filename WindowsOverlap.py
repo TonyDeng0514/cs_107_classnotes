6b38e459026aef524ba3c8f47bf7fcb155fdf4ce
TonyDeng0514 <89807470+TonyDeng0514@users.noreply.github.com>
2021-09-03 13:30:49 -0400

    Update WindowsOverlap.py

--- WindowsOverlap.py
+++ WindowsOverlap.py
@@ -8,9 +8,15 @@ INSERT TEST SUITE HERE
 from Logic import *
 
 def windowOverlap(xmin1: float, xmax1: float, ymin1: float, ymax1: float, xmin2: float, xmax2: float, ymin2: float, ymax2: float) -> bool:
-    precondition(xmin1 <= xmax1 and xmin2 <= xmax2 and ymin1 <= ymax1 and ymin2 <= ymax2)
+    assert (xmin1 <= xmax1 and xmin2 <= xmax2 and ymin1 <= ymax1 and ymin2 <= ymax2) # precondition
     """postcondition: return true iff there exists x such that min1 <= x <= max1 and min2 <= x <= max2 """
-    return False # FIX
+    if xmax1 >= xmin2 and xmax2 >= xmin1:
+        if ymax1 >= ymin2 and ymax2 >= ymin1:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 # The following gets the "doctest" system to check test cases in the documentation comments
 def _test():
