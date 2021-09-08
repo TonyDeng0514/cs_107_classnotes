561b4e4e44f44097dbf13556ca50de633ea98c50
TonyDeng0514 <89807470+TonyDeng0514@users.noreply.github.com>
2021-09-08 12:16:10 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,23 @@
+class Student:
+   Total= 0
+   def __init__(self, name="Adam", id="123456789"):
+       self.name = name
+       self.ID = id
+       Student.Total += 1
+   def displayCount(self):
+       print ("Total students %d" % Student.Total)
+   def displayStudentDetails(self):
+       print ("Name: ", self.name, ", ID: ", self.ID)
+
+if __name__ == "__main__":
+    Student1 = Student()
+    Student1.displayStudentDetails()
+    Student1.displayCount()
+
+    Student2 = Student("Alice", 23456789876)
+    Student2.displayStudentDetails()
+    Student2.displayCount()
+   
+    Student3 = Student(id=23456789876, name="Bob")
+    Student3.displayStudentDetails()
+    Student3.displayCount()
