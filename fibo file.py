283b97c7f16efcf347e01d86dd70b860fcf86840
TonyDeng0514 <dyt20010514@gmail.com>
2021-09-22 12:51:44 -0400

    9/22 by Tony

--- fibo file.py
+++ fibo file.py
@@ -1,3 +1,4 @@
+'''
 N = 7
 fibonacciSeries = [0,1]
 if N>2:
@@ -18,3 +19,19 @@ def fib(n: int)->int:
         prev = answer
         answer = prev + prev2
     return answer
+'''
+
+def fib(n: int)->int:
+    
+    def fib_helper(n:int, answer:int, prev:int, prev2:int, i:int)-> int:
+        if i >= n:
+            return answer
+        else:
+            return fib_helper(n, prev+answer, answer, prev, i+1)
+        
+    
+    assert n >= 0
+
+    return fib_helper(n, 1, 1, 1, 1)
+
+print(fib(7))
