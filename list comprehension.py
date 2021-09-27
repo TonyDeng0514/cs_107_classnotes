a7a20004cc58ddc9cb9434a3ae4d7e9e2f134b06
TonyDeng0514 <dyt20010514@gmail.com>
2021-09-27 12:58:51 -0400

    9/27

--- list comprehension.py
+++ list comprehension.py
@@ -1,3 +1,15 @@
+# for loop
+temps = [100.0, 98.6, 78.34, 88.5, 34.2]
+windChillRate = 0.9
+
+lst = []
+
+for i in range(len(temps)):
+    lst.append(temps[i] *(1+ windChillRate))
+    
+print(lst)
+
+
 # map()
 temps = [100.0, 98.6, 78.34, 88.5, 34.2]
 windChillRate = 0.9
@@ -7,3 +19,14 @@ def getWindChills(temps):
 
 final_temps = map(getWindChills, temps)
 list(final_temps)
+print(list(final_temps))
+
+# list comprehension
+temps = [100.0, 98.6, 78.34, 88.5, 34.2]
+windChillRate = 0.9
+
+lst = [x*(1 + windChillRate) for x in temps]
+    
+print(lst)
+
+
