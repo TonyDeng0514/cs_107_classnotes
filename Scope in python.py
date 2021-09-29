2d58e3916c424fc899ff8fc406a17aed94dc732e
TonyDeng0514 <89807470+TonyDeng0514@users.noreply.github.com>
2021-09-29 11:53:19 -0400

    Create Scope in python.py

---
+++ Scope in python.py
@@ -0,0 +1,20 @@
+#What would be the output of this code ?
+x = 100
+print("1. Global x:", x)
+class Test():
+    x = x + 100 # x is local and class variable
+    y = 300
+    def __init__(self):
+        self.x = 500 # instance variable i.e. belongs to object
+        print("2. Global x:", x)
+        print("3. Class x:", Test.x)
+        print("4. Object x:", self.x)
+        try:
+            print(y)  # Trying to access y which is not defined
+            # self.y is accessible
+        except NameError as e:
+            print("5.", e)  
+            
+# Driver code
+Object = Test()
+DemoLink
