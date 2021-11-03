7698adb4c7231414db1a1e11b8bb344b368532f8
TonyDeng0514 <dyt20010514@gmail.com>
2021-11-03 11:54:27 -0400

    done in class

--- 11-3.py
+++ 11-3.py
@@ -1,2 +1,42 @@
+'''
+Test cases:
+
+>>> assert anagram('cat','act')
+>>> assert anagram('Bored', 'Robed')
+>>> assert anagram('Save', 'Vase')
+'''
+
+
 # anagram
 
+def anagram(a: str, b: str) -> bool:
+    
+
+    a = list(a.lower())
+    b = list(b.lower())
+
+    for i in a:
+        if i == " ":
+            a.remove(i)
+    for i in b:
+        if i == " ":
+            b.remove(i)
+
+    if len(a) != len(b):
+        return False
+    else:
+        for i in a:
+            for j in b:
+                if i == j:
+                    b.remove(j)
+    
+    if b != []:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    print("doctests completed")
