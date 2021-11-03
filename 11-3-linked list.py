a089fa567f8e6d24b2aba69b90f5345efb7ca6b9
TonyDeng0514 <89807470+TonyDeng0514@users.noreply.github.com>
2021-11-03 12:12:42 -0400

    Create 11-3-linked list.py

---
+++ 11-3-linked list.py
@@ -0,0 +1,27 @@
+class Node:
+   def __init__(self, x = None):
+      self.val = x
+      self.next = None
+
+      
+class listADT:
+    def __init__(self, head = None, rest = None):
+        if head == None: # empty constructor
+		
+        else:           # extending constructor
+
+    def empty(self):
+        pass
+
+    def head(self):
+        pass
+
+    def rest(self):
+        pass
+
+	# insert what at position where starting at 0
+    def insert(self, what, where):
+        pass
+
+    def remove(self, x):  # mutate list to remove x if there; 1st found
+        pass
