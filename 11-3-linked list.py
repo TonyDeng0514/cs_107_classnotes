51daad36290646fe774c8a1c7663dc256bd9912a
TonyDeng0514 <dyt20010514@gmail.com>
2021-11-03 16:36:26 -0400

    JD did something to it

--- 11-3-linked list.py
+++ 11-3-linked list.py
@@ -4,20 +4,27 @@ class Node:
       self.next = None
 
       
-class listADT:
+class linkedlistADT:
     def __init__(self, head = None, rest = None):
         if head == None: # empty constructor
-		
+            self.rep = None
         else:           # extending constructor
+            assert type(rest) == linkedlistADT
+            temp = Node(head)
+            temp.next = rest.rep
+            self.rep = temp
 
     def empty(self):
-        pass
+        return self.rep == None
 
     def head(self):
-        pass
+        return self.rep.val
 
     def rest(self):
-        pass
+        assert not self.empty()
+        temp = linkedlistADT()
+        temp.rep = self.rep.next
+        return temp
 
 	# insert what at position where starting at 0
     def insert(self, what, where):
